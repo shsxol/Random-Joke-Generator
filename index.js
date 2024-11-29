394032f65aa83685f61275772ed08a9eab621fdd
shsxol <shrestha.smile31@gmail.com>
2024-11-29 18:23:26 +0545

    fetch api added

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const generateButton= document.querySelector('.generate-button');
+const memeTitle = document.querySelector('.meme-title');
+const memeUrl = document.querySelector('.meme-img');
+const memeAuthor = document.querySelector('.author');
+
+function getMeme(){
+    fetch("https://meme-api.com/gimme/wholesomememes")
+    .then((res) => res.json())
+    .then((data) =>{
+        const {author,title,url} = data
+        memeAuthor.innerHTML = `Meme by : ${author}`;
+        memeTitle.innerHTML = title;
+        memeUrl.src = url;
+        console.log(author,title,url);
+    })
+}
+getMeme();
+
+generateButton.addEventListener('click', () =>{
+    getMeme();
+})
 
